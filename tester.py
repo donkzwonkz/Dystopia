341911e87f4e3e9c7347a6a1cd96e53803d0cb21
keane tan <donkzwonkz@gmail.com>
2020-03-01 21:15:50 +0800

    Fork this

---
+++ tester.py
@@ -0,0 +1,17 @@
+import backend
+from backend import preprocess_image, extract_faces, add_face, remove_face, identify_face, identify_all, facedetect
+import requests
+
+image=preprocess_image("tham caffeine.png")
+image=extract_faces(image)
+label="Tham"
+
+data={
+    'label':label,
+    'image':image
+}
+url="http://127.0.0.1:5000"
+#url="http://httpbin.org/post"
+def check_requests:
+ requests.post(url,data=data,timeout=2.5)
+print(check_requests)
